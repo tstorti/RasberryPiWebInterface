f18029c6866288878a108d64508af5c7333c059e
Tony Storti <tonystorti@gmail.com>
2017-07-10 16:28:20 -0500

    initial commit and vue setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var tempMonitor = new Vue({
+	el:"#tempMonitor",
+	data:{
+		appName: "Temperature Monitor",
+		//for isOnlineColor class to app header
+		isOnline:false,
+		header1: "header1",
+	},
+});
