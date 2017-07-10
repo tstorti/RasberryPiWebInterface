18cee001d58fb8a67fe5635f0cbd89d0ef68cb27
Tony Storti <tonystorti@gmail.com>
2017-07-10 17:04:53 -0500

    added basic inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,13 @@ var tempMonitor = new Vue({
 	el:"#tempMonitor",
 	data:{
 		appName: "Temperature Monitor",
-		//for isOnlineColor class to app header
+		userName: '',
 		isOnline:false,
 		header1: "header1",
+		displayOptions:[],
+		tempHeader: "Temperature: ",
+		tempValue: 50,
+		humidityHeader:"Humidity: ",
+		humidityValue: "45%",
 	},
 });
