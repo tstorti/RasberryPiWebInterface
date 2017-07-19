57600e5beb0fa95c0213ce0d20e17564028b1182
Tony Storti <tonystorti@gmail.com>
2017-07-19 16:07:24 -0500

    connected to firebase so interface can start/stop data collection

--- js/app.js
+++ js/app.js
@@ -22,13 +22,14 @@ var tempMonitor = new Vue({
 		showHistoric:false,
 		showSession:false,
 		historicData:null,
-		sessionData:null,
+		sessionData:[],
 		timeInterval:null,
 		newTimestampArray:[],
 		newTempArray:[],
 		newHumidityArray:[],
 		tempChart:null,
 		humidityChart:null,
+		firebaseKey:null,
 	},
 	methods:{
 		displayCurrentInfo:function(){
@@ -42,34 +43,53 @@ var tempMonitor = new Vue({
 				this.statusBtnText="Start Collecting Data";
 				this.isOnline=false;
 				clearInterval(tempMonitor.timeInterval);
+				firebase.database().ref().child("status").update({
+					"isRecording":false,
+				});
 			}
 			
-			//show updated data every 5 seconds and log it to the newData array
 			if(tempMonitor.appState===1){
-				tempMonitor.timeInterval=window.setInterval(function(){
-						//placeholders for now - will reset to input from rasberry pi api
-						tempMonitor.tempValue = Math.floor(Math.random()*20)+50;
-						tempMonitor.humidityValue = Math.floor(Math.random()*100);
-						tempMonitor.soilMoistureValue = Math.floor(Math.random()*100);
-						var timestamp= moment().format("LTS");
-						var datestamp=moment().format('L')
+				//reset session data arrays so duplicate data isn't appended when user clicks collect data button 2nd time.
+				tempMonitor.newTimestampArray=[];
+				tempMonitor.newTempArray=[];
+				tempMonitor.newHumidityArray=[];
+				tempMonitor.sessionData=[];
+
+				//get new key to save current session data
+				firebaseKey = firebase.database().ref().child("historic_sessions").push().getKey();
+				//if not set to "off", rasberry pi will record data.
+				firebase.database().ref().child("status").update({
+					"current_session":firebaseKey,
+					"isRecording":true,
+				});
+				//set initial session data
+				firebase.database().ref().child("historic_sessions").child(firebaseKey).set({
+					"name": tempMonitor.sessionName,
+					"start_time": moment().format("YYYY-MM-DD HH:mm:ss"),
+				});
+				//using historic data placeholder for now - need to replace child key with "firebaseKey"
+				firebase.database().ref().child("historic_sessions").child("-KpRXX9lJgH8d3Og530e").child("data").on("child_added", function(snapshot) {
+					
+						tempMonitor.tempValue = snapshot.val().temperature;
+						tempMonitor.humidityValue = snapshot.val().humidity;
+						tempMonitor.soilMoistureValue = snapshot.val().pressure;
+						timestamp= snapshot.val().timestamp;
 						
 						var newRecord = {
 							"session_name":tempMonitor.sessionName,
-							"date":datestamp,
 							"time":timestamp,
 							"temp":tempMonitor.tempValue,
 							"humidity":tempMonitor.humidityValue,
 							"soilMoisture":tempMonitor.soilMoistureValue,
 						};
-						newData.push(newRecord);
+						tempMonitor.sessionData.push(newRecord);
 						tempMonitor.newTimestampArray.push(timestamp);
 						tempMonitor.newTempArray.push(tempMonitor.tempValue);
 						tempMonitor.newHumidityArray.push(tempMonitor.humidityValue);
 						Chart.defaults.global.animation=false;
 						tempMonitor.sessionTempChart();
 						tempMonitor.sessionHumidityChart();			
-				},5000);
+				});
 			}
 		},
 		
@@ -89,7 +109,6 @@ var tempMonitor = new Vue({
 			if(this.showSession===false){
 				this.showSession=true;
 				this.sessionToggleText="Hide Session Data";
-				this.sessionData=newData;
 				this.sessionTempChart();
 				this.sessionHumidityChart();
 			}
@@ -99,15 +118,16 @@ var tempMonitor = new Vue({
 			}
 			
 		},
-		sessionTempChart:function(){
+		sessionTempChart:function(){		
+			
 			var option={
 				responsive:true,
 			};
 			var data = {
-			    labels: this.newTimestampArray,
+			    labels: this.limitLabels(this.newTimestampArray),
 			    datasets: [
 			        {
-			            label: "My Second dataset",
+			            label: "Temp Chart",
 			            fillColor: "rgba(151,187,205,0.2)",
 			            strokeColor: "rgba(151,187,205,1)",
 			            pointColor: "rgba(151,187,205,1)",
@@ -120,16 +140,17 @@ var tempMonitor = new Vue({
 			};
 			var ctx = document.getElementById("sessionTempChart").getContext('2d');
     		this.tempChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
+
 		},
 		sessionHumidityChart:function(){
 			var option={
 				responsive:true,
 			};
 			var data = {
-			    labels: this.newTimestampArray,
+			    labels: this.limitLabels(this.newTimestampArray),
 			    datasets: [
 			        {
-			            label: "My First dataset",
+			            label: "Humidity Chart",
 			            fillColor: "rgba(220,220,220,0.2)",
 			            strokeColor: "rgba(220,220,220,1)",
 			            pointColor: "rgba(220,220,220,1)",
@@ -143,5 +164,34 @@ var tempMonitor = new Vue({
 			var ctx = document.getElementById("sessionHumidityChart").getContext('2d');
     		this.humidityChart = new Chart(ctx).Line(data, option); //'Line' defines type of the chart.
 		},
+		//only show 5 labels for array of time after large number of data points for readability
+		limitLabels:function(array){
+			if(array.length>12){
+				var firstLabel = array[0];
+				var lastLabel = array[array.length -1];
+				var label25 = array[Math.floor(array.length/4)];
+				var label50 = array[Math.floor((array.length/4)*2)];
+				var label75 = array[Math.floor((array.length/4)*3)];
+				var newLabelArray = [];
+
+				for (var i=0;i<this.newTimestampArray.length;i++){
+					newLabelArray[i]="";
+				}
+				newLabelArray[0]=firstLabel;
+				newLabelArray[Math.floor(array.length/4)]=label25;
+				newLabelArray[Math.floor(array.length/4)*2]=label50;
+				newLabelArray[Math.floor(array.length/4)*3]=label75;
+				newLabelArray[array.length -1]=lastLabel;
+			}
+			else{
+				var newLabelArray = array;
+			}
+			return (newLabelArray);	
+		},
+		initFirebase:function(){
+			firebase.initializeApp(keys.firebaseKeys);
+		},
 	},
 });
+
+firebase.initializeApp(firebaseKeys);
--- js/data.js
+++ js/data.js
@@ -55,5 +55,5 @@ var testData ={
 		},
 	},
 }; 
-var newData=[];
+
 
